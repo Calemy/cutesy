6aeaf7bc224b2f618e7cf5fae8e14a5439b3642a
HorizonCode <horizoncode88@gmail.com>
2022-08-03 14:00:32 +0200

    update typescript definitions

--- index.d.ts
+++ index.d.ts
@@ -1,51 +1,8 @@
 export = class Logger {
-    constructor();
-
-    addTimestamp(format: "hh:mm:ss" | "hh:mm" | "mm:ss"): Logger
-
-    changeColor(code: number): Logger
-
-    changeFont(code: number): Logger
-
-    changeTag(tag: string): Logger
-
-    save(path: string, message?: string): Logger
-
-    send(message: string): Logger
-
-    sendTraced(message: string, nesting?: number): Logger
-
-    black(): Logger
-
-    white(): Logger
-
-    red(): Logger
-
-    green(): Logger
-    
-    darkGreen(): Logger
-
-    darkBlue(): Logger
-
-    blue(): Logger
-
-    cyan(): Logger
-
-    lightBlue(): Logger
-
-    purple(): Logger
-
-    lightPurple(): Logger
-
-    yellow(): Logger
-
-    pink(): Logger
-
-    bold(): Logger
-
-    underline(): Logger
-
-    reversed(): Logger
-
-    reset(): Logger
+    constructor(appName: string);
+    log(message: string, logType: "INFO" | "WARNING" | "ERROR" | "DEBUG"): void;
+    logTraced(message: string, logType: "INFO" | "WARNING" | "ERROR" | "DEBUG", nested: number): void;
+    setDateTimeFormat(dateTimeFormat: string): void;
+    setDebug(debug: boolean): void;
+    setAppName(appName: string): void;
 }
